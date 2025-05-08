2e454537442b84f2a0a2a8334572d8931623eca8
Mario Fraire <fraire.tepox.mario.valentin@gmail.com>
2025-05-08 11:41:07 -0600

    cambios en la carpeta

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  holas sdas
+  asdsa
+  <div class="container mt-5">
+    <div class="card">
+      <div class="card-body">
+        <correo></correo>
+      </div>
+    </div>
+
+  </div>
+
 </template>
+
+<script setup>
+
+import correo from './stores/correo.vue';
+
+
+</script>
